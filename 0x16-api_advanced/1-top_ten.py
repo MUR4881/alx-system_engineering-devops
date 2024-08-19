4c3f8d355420c9a80afd534c7fd43adf14d7007f
ALIYU <usmanmurabi@gmail.com>
2024-08-19 17:13:49 +0100

    Delete Directory Soon

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-'''Module containing subreddit api tasks'''
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
+
 def top_ten(subreddit):
-    '''
-    rhis queries the Reddit API and prints the titles of the first 10 hot posts
-    listed for a given subreddit.
-    If an invalid subreddit is given, the function should print None.
-    '''
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-            "User-Agent": "linux:0x16.api.advanced:v1.0.0"
-            }
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
     params = {
         "limit": 10
     }
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
-    if response.status_code == 200:
-        results = response.json().get("data")
-        [print(item.get("data").get("title")) for item in results.get("children")]
-    else:
+    if response.status_code == 404:
         print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
